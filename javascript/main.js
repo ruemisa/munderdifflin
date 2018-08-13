d6b92f911ef22c36108b5285cb2a3a5df8c98afe
Ruel Misa <ruel.g.misa@gmail.com>
2018-08-13 00:33:08 -0400

    Made everything as responsive as possible and cleaned up code

--- javascript/main.js
+++ javascript/main.js
@@ -13,44 +13,58 @@ let closeMenu = () => {
 // Slide in Right catchphrase
 
 let firstAnimate = () => {
+
   let catchText = document.getElementById('catch-txt');
   catchText.classList.remove('hidden');
   catchText.classList.add('slide-right');
+
   let contact = document.getElementById('catch-btn');
-  contact.classList.remove('hidden');
   contact.classList.add('zoom-in');
 }
 
 
 // Scroll Event to load about page
 
+const screenWidth = window.innerWidth;
+const about = document.getElementById('about');
+const client = document.getElementById('client');
+const target = document.getElementById('target');
+const events = document.getElementById('event-txt');
+
 let scrollDown = () => {
   let scrollPos = window.scrollY;
-  if (scrollPos > 30 && scrollPos <= 80) {
-    // console.log(scrollPos);
-    const about = document.getElementById('about');
+  
+  if (screenWidth > 768) {
+    if (scrollPos > 30 && scrollPos <= 80) {
+
+      about.classList.remove('hidden');
+      about.classList.add('fade-in-up');
+  
+    } else if (scrollPos > 400 && scrollPos <= 600) {
+  
+      client.classList.remove('hidden');
+      client.classList.add('roll-in');
+  
+    } else if (scrollPos > 700 && scrollPos <= 1000) {
+  
+      target.classList.remove('hidden');
+      target.classList.add('fade-in');
+  
+    } else if (scrollPos > 1300 ) {
+  
+      events.classList.remove('hidden');
+      events.classList.add('fade-in-left');
+    }
+  } else {
     about.classList.remove('hidden');
-    about.classList.add('fade-in-up');
-  } else if (scrollPos > 400 && scrollPos <= 600) {
-    // console.log(scrollPos);
-    const client = document.getElementById('client');
     client.classList.remove('hidden');
-    client.classList.add('roll-in');
-  } else if (scrollPos > 700 && scrollPos <= 1000) {
-    // console.log(scrollPos);
-    const target = document.getElementById('target');
     target.classList.remove('hidden');
-    target.classList.add('fade-in');
-  } else if (scrollPos > 1300 ) {
-    // console.log(scrollPos);
-    const events = document.getElementById('event-txt');
     events.classList.remove('hidden');
-    events.classList.add('fade-in-left');
   }
+
 }
 
-window.addEventListener('scroll', scrollDown);
-
+  window.addEventListener('scroll', scrollDown);
 
 // EVENTS AREA FUNCTION
 
